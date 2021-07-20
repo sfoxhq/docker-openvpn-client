32eb93e11bb8fabad0a5f2d28183cc76df458087
Leonardo Cicconi <6304835+ldcicconi@users.noreply.github.com>
2021-07-19 17:01:33 -0700

    use ash

--- data/scripts/entry.sh
+++ data/scripts/entry.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/ash
+# shellcheck shell=ash
+# shellcheck disable=SC2169 # making up for lack of ash support
 
 cleanup() {
     # When you run `docker stop` or any equivalent, a SIGTERM signal is sent to PID 1.
